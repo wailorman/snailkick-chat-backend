f9854f1e5f27dd84b0ca48a407b7ee90e37b5117
wailorman <wailorman@gmail.com>
2015-03-06 21:05:36 +0300

    + attach client to request module

--- modules/clients-module.js
+++ modules/clients-module.js
@@ -6,19 +6,23 @@ var mongoose = require( 'mongoose' ),
 
     Client   = require( '../objects/client/client.js' );
 
-
+/**
+ * Find Client by id or token
+ * @param req id|token
+ * @param res
+ * @param next
+ */
 var getClient = function ( req, res, next ) {
 
-   var query = {};
+    var query = {};
 
-    if ( ! req.params.id ) return next( new restify.InvalidArgumentError( 'id require' ) );
+    if ( !req.params.id ) return next( new restify.InvalidArgumentError( 'id require' ) );
 
-    if ( ! mf.isObjectId( req.params.id ) && ! mf.isToken( req.params.id ) ) return next( new restify.InvalidArgumentError( 'invalid id' ) );
+    if ( !mf.isObjectId( req.params.id ) && !mf.isToken( req.params.id ) ) return next( new restify.InvalidArgumentError( 'invalid id' ) );
 
 
     if ( mf.isToken( req.params.id ) ) query = { token: req.params.id };
     else if ( mf.isObjectId( req.params.id ) ) query = { id: req.params.id };
-
 
 
     var resultClient = new Client();
@@ -37,4 +41,26 @@ var getClient = function ( req, res, next ) {
 
 };
 
+/**
+ * If req.params.token exist, this middleware will create req.client
+ */
+var attachClientToRequest = function ( req, res, next ) {
+
+    if ( !req.params.token ) return next();
+
+    var client = new Client();
+
+    client.findOne( { token: req.params.token }, function ( err ) {
+
+        if ( err ) return next( new restify.ForbiddenError( "Can't find Client with such token!" ) );
+
+        req.client = client;
+
+        return next();
+
+    } );
+
+};
+
 module.exports.getClient = getClient;
+module.exports.attachClientToRequest = attachClientToRequest;
